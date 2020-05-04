a38508ec88c97858e5a24ea659f6408fa15c161d
Jolisa Tran <jolisa.chan.store@gmail.com>
2020-05-04 01:10:54 -0400

    search and create venues

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from flask_wtf import FlaskForm
 from forms import *
 import sys
 from flask_migrate import Migrate
+from sqlalchemy import func
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -28,11 +29,23 @@ migrate = Migrate(app,db)
 # Models.
 #----------------------------------------------------------------------------#
 
-# Show is a middle table of many-to-many relationship. Venue is Parent. Artist is Child
-shows = db.Table('shows',
-    db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True),
-    db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True)
-)
+# shows = db.Table('shows',
+#     db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True),
+#     db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True),
+#     db.Column('start_time',db.DateTime) 
+# )
+
+class Shows(db.Model):
+    __tablename__ = 'shows'
+
+    id = db.Column('id', db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'),nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'),nullable=False)
+    start_time = db.Column(db.DateTime, nullable=True, default=func.now())
+
+    artist= db.relationship("Artist")
+    def __repr__(self):
+      return f'<Shows {self.venue_id} {self.artist_id} {self.start_time}>'
 
 class Venue(db.Model):
     __tablename__ = 'Venue'
@@ -45,9 +58,9 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    artists = db.relationship('Artist', secondary=shows,
-      backref=db.backref('venues', lazy=True))
-
+    # artists = db.relationship('Artist', secondary=shows,
+    #   backref=db.backref('venues', lazy=True))
+    #shows = db.relationship('Shows',backref=db.backref('Shows',lazy=True))
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
     def __repr__(self):
       return f'<Venue {self.id} {self.name}>'
@@ -69,7 +82,7 @@ class Artist(db.Model):
       return f'<Artist {self.id} {self.name}>'
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-
+# look at the null condition and what is set keys : 1 venue,1 artist can have multi show dates?
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -101,127 +114,80 @@ def index():
 def venues():
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
-  }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }]
-  return render_template('pages/venues.html', areas=data);
+    datas=db.session.query(func.count(Venue.id).label('num_id'),Venue.city,Venue.state ).group_by( Venue.city, Venue.state).all()
+    stmt = db.session.query(Shows.venue_id,func.count('*').label('num_shows_count')).filter(Shows.start_time>datetime.datetime.now()).group_by(Shows.venue_id).subquery()
+    # query= db.session.query(Venue.id,Venue.name,stmt.c.num_shows_count).outerjoin(stmt,Venue.id==stmt.c.venue_id).filter(Venue.city==data.city,Venue.state==data.state).all()
+    results = [{
+            
+                "city": data.city,
+                "state": data.state,                
+                "venues": db.session.query(Venue.id,Venue.name,stmt.c.num_shows_count).outerjoin(stmt,Venue.id==stmt.c.venue_id).filter(Venue.city==data.city,Venue.state==data.state).all()           
+                
+                } for data in datas]
+    
+    return render_template('pages/venues.html', areas=results)
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for Hop should return "The Musical Hop".
-  # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-  response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_venues.html', results=response, search_term=request.FlaskForm.get('search_term', ''))
+  try:
+    search_term=request.form.get('search_term', '')
+    stmt = db.session.query(Shows.venue_id,func.count('*').label('num_shows_count')).filter(Shows.start_time>datetime.datetime.now()).group_by(Shows.venue_id).subquery()
+    query=db.session.query(Venue).filter(func.lower(Venue.name).contains(search_term.lower(), autoescape=True) )
+    datas = query.all()
+    response={
+      "count": query.count(),
+      "data": [{
+        "id": data.id,
+        "name": data.name,
+        "num_upcoming_shows": 0,#TODO:added stmt but no connection
+      } for data in datas]
+    }
+    return render_template('pages/search_venues.html', results=response,search_term=request.form.get('search_term', ''))
+  except Exception as e:
+    flash(e)
+    return render_template('errors/500.html')
+  
 
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
   # shows the venue page with the given venue_id
   # TODO: replace with real venue data from the venues table, using venue_id
-  data1={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-    "past_shows": [{
-      "artist_id": 4,
-      "artist_name": "Guns N Petals",
-      "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-      "start_time": "2019-05-21T21:30:00.000Z"
-    }],
-    "upcoming_shows": [],
-    "past_shows_count": 1,
-    "upcoming_shows_count": 0,
-  }
-  data2={
-    "id": 2,
-    "name": "The Dueling Pianos Bar",
-    "genres": ["Classical", "R&B", "Hip-Hop"],
-    "address": "335 Delancey Street",
-    "city": "New York",
-    "state": "NY",
-    "phone": "914-003-1132",
-    "website": "https://www.theduelingpianos.com",
-    "facebook_link": "https://www.facebook.com/theduelingpianos",
-    "seeking_talent": False,
-    "image_link": "https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-    "past_shows": [],
-    "upcoming_shows": [],
-    "past_shows_count": 0,
-    "upcoming_shows_count": 0,
-  }
-  data3={
-    "id": 3,
-    "name": "Park Square Live Music & Coffee",
-    "genres": ["Rock n Roll", "Jazz", "Classical", "Folk"],
-    "address": "34 Whiskey Moore Ave",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "415-000-1234",
-    "website": "https://www.parksquarelivemusicandcoffee.com",
-    "facebook_link": "https://www.facebook.com/ParkSquareLiveMusicAndCoffee",
-    "seeking_talent": False,
-    "image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-    "past_shows": [{
-      "artist_id": 5,
-      "artist_name": "Matt Quevedo",
-      "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-      "start_time": "2019-06-15T23:00:00.000Z"
-    }],
-    "upcoming_shows": [{
-      "artist_id": 6,
-      "artist_name": "The Wild Sax Band",
-      "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-      "start_time": "2035-04-01T20:00:00.000Z"
-    }, {
-      "artist_id": 6,
-      "artist_name": "The Wild Sax Band",
-      "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-      "start_time": "2035-04-08T20:00:00.000Z"
-    }, {
-      "artist_id": 6,
-      "artist_name": "The Wild Sax Band",
-      "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-      "start_time": "2035-04-15T20:00:00.000Z"
-    }],
-    "past_shows_count": 1,
-    "upcoming_shows_count": 1,
-  }
-  data = list(filter(lambda d: d['id'] == venue_id, [data1, data2, data3]))[0]
-  return render_template('pages/show_venue.html', venue=data)
+  try:
+    
+    upcoming = db.session.query(Shows.venue_id,func.count('*').label('num_shows_count')).filter(Shows.start_time>datetime.datetime.now()).group_by(Shows.venue_id).subquery()
+    query=db.session.query(Venue)
+    datas = query.all()
+    results = [{      
+      {
+        "id": data.id,
+        "name": data.name,
+        "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
+        "address": "1015 Folsom Street",
+        "city": "San Francisco",
+        "state": "CA",
+        "phone": "123-123-1234",
+        "website": "https://www.themusicalhop.com",
+        "facebook_link": "https://www.facebook.com/TheMusicalHop",
+        "seeking_talent": True,
+        "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
+        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        "past_shows": [{
+          "artist_id": 4,
+          "artist_name": "Guns N Petals",
+          "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+          "start_time": "2019-05-21T21:30:00.000Z"
+        }],
+        "upcoming_shows": [],
+        "past_shows_count": 1,
+        "upcoming_shows_count": 0,
+      } for data in datas
+    }]
+    #TODO: convert to a list like below by using venue_id
+    #data = list(filter(lambda d: d['id'] == venue_id, [data1, data2, data3]))[0]
+    return render_template('pages/show_venue.html', venue=data1)
+  except Exception as e:
+    flash(e)
+    return render_template('errors/500.html')
 
 #  Create Venue
 #  ----------------------------------------------------------------
@@ -251,32 +217,45 @@ def create_venue_submission():
     db.session.commit()
     # TODO: modify data to be the data object returned from db insertion
     body['name'] = venue.name
-  except:
-    error = True
-    db.session.rollback()
-    print(sys.exc_info())
-  finally:
-    db.session.close()
-  if not error:
-    # on successful db insert, flash success
     flash('Venue ' + venue.name + ' was successfully listed!')
     return jsonify(body)
-  else:
-    # TODO: on unsuccessful db insert, flash an error instead.
-    #flash('An error occurred. Venue ' + venue.name + ' could not be listed.')
-    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+  except Exception as e:
+    flash(e)
+    db.session.rollback()
     return render_template('errors/500.html')
+  finally:
+    db.session.close()
+  # if not error:
+  #   # on successful db insert, flash success
+  #   print("here 1")
+  #   flash('Venue ' + venue.name + ' was successfully listed!')
+    
+  # else:
+  #   print("here 2")
+  #   # TODO: on unsuccessful db insert, flash an error instead.
+  #   flash('An error occurred. Venue could not be listed.')
+  #   # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+  #   return None #render_template('errors/500.html')
   
-  return render_template('pages/home.html')
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
+#https://docs.sqlalchemy.org/en/13/orm/basic_relationships.html#deleting-rows-from-the-many-to-many-table
 def delete_venue(venue_id):
   # TODO: Complete this endpoint for taking a venue_id, and using
   # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
 
   # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
   # clicking that button delete it from the db then redirect the user to the homepage
+  flash("deleted")
   return None
+  # try:
+  #   Venue.query.filter_by(id=venue_id).delete()
+  #   db.session.commit()
+  # except:
+  #   db.session.rollback()
+  # finally:
+  #   db.session.close()
+  # return jsonify({ 'success': True })
 
 #  Artists
 #  ----------------------------------------------------------------
@@ -297,18 +276,23 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+  try:
+    search_term=request.form.get('search_term', '')
+    
+    query=db.session.query(Artist).filter(func.lower(Artist.name).contains(search_term.lower(), autoescape=True) )
+    datas = query.all()
+    response={
+      "count": query.count(),
+      "data": [{
+        "id": data.id,
+        "name": data.name,
+        "num_upcoming_shows": 0,
+      } for data in datas]
+    }
+    return render_template('pages/search_artists.html', results=response,search_term=request.form.get('search_term', ''))
+  except Exception as e:
+    flash(e)
+    return render_template('errors/500.html')
 
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
--- static/js/self.js
+++ static/js/self.js
@@ -1,6 +1,7 @@
 //create venue
-document.getElementById('create-venue-form').onsubmit = function(e) {
-    e.preventDefault();
+$("#createVenue").on("click", function(e) {
+    //document.getElementById('create-venue-form').onsubmit = function(e) {
+    //e.preventDefault();
     const name = document.getElementById('name').value;
     const city = document.getElementById('city').value;
     const state = document.getElementById('state').value;
@@ -8,7 +9,6 @@ document.getElementById('create-venue-form').onsubmit = function(e) {
     const phone = document.getElementById('phone').value;
     const image_link = document.getElementById('image_link').value;
     const facebook_link = document.getElementById('facebook_link').value;
-
     fetch('/venues/create', {
             method: 'POST',
             body: JSON.stringify({
@@ -28,68 +28,84 @@ document.getElementById('create-venue-form').onsubmit = function(e) {
         .then(jsonResponse => {
             console.log(jsonResponse);
         })
-        .catch(function(e) {
-            console.log('Error occurred ');
+        .catch(function(error) {
+            console.log(error);
         });
-}
+})
 
 //create artist
-document.getElementById('create-artist-form').onsubmit = function(e) {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    const city = document.getElementById('city').value;
-    const state = document.getElementById('state').value;
-    const address = document.getElementById('address').value;
-    const phone = document.getElementById('phone').value;
-    const genres = document.getElementById('genres').value;
-    const image_link = document.getElementById('image_link').value;
-    const facebook_link = document.getElementById('facebook_link').value;
+// function createArtist(e) {
+//     //document.getElementById('create-artist-form').onsubmit = function(e) {
+//     e.preventDefault();
+//     const name = document.getElementById('name').value;
+//     const city = document.getElementById('city').value;
+//     const state = document.getElementById('state').value;
+//     const address = document.getElementById('address').value;
+//     const phone = document.getElementById('phone').value;
+//     const genres = document.getElementById('genres').value;
+//     const image_link = document.getElementById('image_link').value;
+//     const facebook_link = document.getElementById('facebook_link').value;
 
-    fetch('/artists/create', {
-            method: 'POST',
-            body: JSON.stringify({
-                'name': name,
-                'city': city,
-                'state': state,
-                'address': address,
-                'phone': phone,
-                'genres': genres,
-                'image_link': image_link,
-                'facebook_link': facebook_link
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
-        .then(response => response.json())
-        .then(jsonResponse => {
-            console.log(jsonResponse);
-        })
-        .catch(function(e) {
-            console.log('Error occurred ');
-        });
-}
+//     fetch('/artists/create', {
+//             method: 'POST',
+//             body: JSON.stringify({
+//                 'name': name,
+//                 'city': city,
+//                 'state': state,
+//                 'address': address,
+//                 'phone': phone,
+//                 'genres': genres,
+//                 'image_link': image_link,
+//                 'facebook_link': facebook_link
+//             }),
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             }
+//         })
+//         .then(response => response.json())
+//         .then(jsonResponse => {
+//             console.log(jsonResponse);
+//         })
+//         .catch(function(e) {
+//             console.log('Error occurred ');
+//         });
+// }
 
-//create show
-document.getElementById('create-show-form').onsubmit = function(e) {
-    e.preventDefault();
-    const venue_id = document.getElementById('venue_id').value;
-    const artist_id = document.getElementById('artist_id').value;
-    fetch('/shows/create', {
-            method: 'POST',
-            body: JSON.stringify({
-                'venue_id': venue_id,
-                'artist_id': artist_id,
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
-        .then(response => response.json())
-        .then(jsonResponse => {
-            console.log(jsonResponse);
-        })
-        .catch(function(e) {
-            console.log('Error occurred ');
-        });
-}
+// //create show
+// function createShow(e) {
+//     //document.getElementById('create-show-form').onsubmit = function(e) {
+//     e.preventDefault();
+//     const venue_id = document.getElementById('venue_id').value;
+//     const artist_id = document.getElementById('artist_id').value;
+//     fetch('/shows/create', {
+//             method: 'POST',
+//             body: JSON.stringify({
+//                 'venue_id': venue_id,
+//                 'artist_id': artist_id,
+//             }),
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             }
+//         })
+//         .then(response => response.json())
+//         .then(jsonResponse => {
+//             console.log(jsonResponse);
+//         })
+//         .catch(function(e) {
+//             console.log('Error occurred ');
+//         });
+// }
+
+
+// // delete venue
+// function deleteItem(e) {
+//     console.log("ok " + $(e).attr("data-id"));
+//     // const venueId = e.target.dataset['id'];
+//     // fetch('/venues/' + venueId, {
+//     //         method: 'DELETE'
+//     //     })
+//     //     .then(function() {
+//     //         const item = e.target.parentElement;
+//     //         item.remove();
+//     //     })
+// }
